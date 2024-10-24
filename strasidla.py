994262b9785631206a01477d3a350df72a076807
Filip Hric <filip@slido.com>
2024-10-24 13:53:58 +0200

    init

---
+++ strasidla.py
@@ -0,0 +1,20 @@
+from random import random
+print("Strašidlá")
+som_hrdina = True
+while som_hrdina:
+    dvere_duch = randint(1, 3)
+    print("Trojo dverípred tebou...")
+    print("Za jednými je duch")
+    print("Ktoré dvere otvoríš?")
+    dvere = input("1, 2 alebo 3?")
+    c_dveri = int(dvere)
+    if c_dveri == dvere_duch
+        print("STRAŠIDLO!")
+        som_hrdina = False
+    else:
+        print("Ziadny duch!")
+        print("Vstupuješ do ďalšej izby")
+        skore = skore + 1
+        print("Tvoje skóre je, ", skore)
+print("Utekaj!")
+print("Koniec hry! Tvoje skóre je,", skore)
